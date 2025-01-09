7c191a98ce8d6ca784170d966312e266ffcdd56a
Aleksander <filipkowskialek@gmail.com>
2025-01-09 22:44:50 +0100

    implemented reading, adding and deleting records

--- HabitTracker.afilipkowski/Interface.cs
+++ HabitTracker.afilipkowski/Interface.cs
@@ -1,72 +1,88 @@
 ﻿using System.Globalization;
 using Database;
 
-namespace HabitTracker
+namespace HabitTracker;
+static class UserInterface
 {
-    static class UserInterface
+    static public void DisplayMenu()
     {
-        static public void DisplayMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to the Habit Tracker app!");
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("1. Display all records");
-            Console.WriteLine("2. Add a new record");
-            Console.WriteLine("3. Update a record");
-            Console.WriteLine("4. Delete a record");
-            Console.WriteLine("5. Exit the app");
-        }
+        Console.Clear();
+        Console.WriteLine("Welcome to the Habit Tracker app!");
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("1. Display all records");
+        Console.WriteLine("2. Add a new record");
+        Console.WriteLine("3. Update a record");
+        Console.WriteLine("4. Delete a record");
+        Console.WriteLine("5. Exit the app");
+    }
 
-        static public bool HandleInput(DatabaseHandler db)
+    static public bool HandleInput(DatabaseHandler db)
+    {
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
         {
-            int choice;
-            while (!int.TryParse(Console.ReadLine(), out choice))
-            {
-                Console.WriteLine("Invalid input. Select an option from the menu.");
-            }
-            switch (choice)
-            {
-                case 1:
-                    return false;
-                case 2:
-                    string? date = GetDate();
-                    int amount = GetAmount();
-                    db.InsertRecord(date, amount);
-                    return false;
-                case 3:
-                    return false;
-                case 4:
-                    return false;
-                case 5:
-                    return true;
-                default:
-                    return false;
-            }
-
+            Console.WriteLine("Invalid input. Select an option from the menu.");
         }
-        static private string? GetDate()
+        switch (choice)
         {
-            string dateInput;
-            Console.WriteLine("Enter the date (dd-mm-yyyy):");
-            dateInput = Console.ReadLine();
-            while (!DateTime.TryParseExact(dateInput, "dd-MM-yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out _))
-            {
-                Console.WriteLine("Invalid input. Enter the date in the correct format.");
-                dateInput = Console.ReadLine();
-            }
-            return dateInput;
-        }
-        static private int GetAmount()
-        {
-            int amount;
-            Console.WriteLine("Enter the number of read pages");
-            while (!int.TryParse(Console.ReadLine(), out amount))
-            {
-                Console.WriteLine("Invalid input. Enter a number.");
-            }
-            return amount;
+            case 1:
+                List<DatabaseRecord> records = db.GetAllRecords();
+                if (records.Count == 0)
+                    Console.WriteLine("No records found");
+                else
+                {
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.Id}. Pages read: {record.Amount}; Date: {record.Date}");
+                    }
+                }
+                return false;
+            case 2:
+                string? date = GetDate();
+                int amount = GetAmount();
+                db.InsertRecord(date, amount);
+                return false;
+            case 3:
+                return false;
+            case 4:
+                int id;
+                int currentRecords = db.GetCount();
+                Console.WriteLine("Enter ID of the record you want to remove.");
+                while (!int.TryParse(Console.ReadLine(), out id) || id > currentRecords || id <= 0)
+                {
+                    Console.WriteLine("Invalid input. Enter correct ID");
+                }
+                db.DeleteRecord(id);
+                return false;
+            case 5:
+                return true;
+            default:
+                return false;
         }
 
     }
+    static private string? GetDate()
+    {
+        string dateInput;
+        Console.WriteLine("Enter the date (dd-mm-yyyy):");
+        dateInput = Console.ReadLine();
+        while (!DateTime.TryParseExact(dateInput, "dd-MM-yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out _))
+        {
+            Console.WriteLine("Invalid input. Enter the date in the correct format.");
+            dateInput = Console.ReadLine();
+        }
+        return dateInput;
+    }
+    static private int GetAmount()
+    {
+        int amount;
+        Console.WriteLine("Enter the number of read pages");
+        while (!int.TryParse(Console.ReadLine(), out amount))
+        {
+            Console.WriteLine("Invalid input. Enter a number.");
+        }
+        return amount;
+    }
+
 }
--- HabitTracker.afilipkowski/Program.cs
+++ HabitTracker.afilipkowski/Program.cs
@@ -1,24 +1,24 @@
 ﻿using Database;
 
-namespace HabitTracker
+namespace HabitTracker;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        bool endApp = false;
+        var db = new DatabaseHandler();
+        while (!endApp)
         {
-            bool endApp = false;
-            var db = new DatabaseHandler();
-            while(!endApp)
-            {
-                UserInterface.DisplayMenu();
-                endApp = UserInterface.HandleInput(db);
-            }
-            Console.WriteLine("Goodbye!");
+            UserInterface.DisplayMenu();
+            endApp = UserInterface.HandleInput(db);
             Console.ReadKey();
-
         }
+        Console.WriteLine("Goodbye!");
+        
+
     }
 }
+
 
 
 
